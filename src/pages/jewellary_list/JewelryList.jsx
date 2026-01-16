26a502928d3d7b9cf263a387c3c1963822e63c59
Gayatri aka Jennifer <46160025+jenniferrexim@users.noreply.github.com>
2026-01-16 15:00:09 +0530

    changes logo and default images

--- src/pages/jewellary_list/JewelryList.jsx
+++ src/pages/jewellary_list/JewelryList.jsx
@@ -487,7 +487,8 @@ const JewelryList = () => {
             src={
               bannerImage
                 ? `${import.meta.env.VITE_BACKEND_URL}/storage/${bannerImage}`
-                : "https://www.dilsejewels.com/cdn/shop/files/Women_s_Diamond_Gemstone_Jewelry_1366x.jpg?v=1729163233"
+                // : "images/Women_s_Diamond_Gemstone_Jewelry_1366x.jpg"
+                : "images/default.jpg"
             }
             alt="banner"
             className="hero-img img-fluid"
